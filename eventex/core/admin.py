e2c00752d594e004d0715dda6aeab10e8be6f1c6
Frederico Araujo <fredericoaraujo@gmail.com>
2017-11-28 22:17:23 -0200

    Contatos

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,20 +1,24 @@
 from django.contrib import admin
 
-from eventex.core.models import Speaker
+from eventex.core.models import Speaker, Contact
+
+
+class ContactInline(admin.TabularInline):
+    model = Contact
+    extra = 1
 
 
 class SpeakerModelAdmin(admin.ModelAdmin):
+    inlines = [ContactInline]
     prepopulated_fields = {'slug': ('name',)}
     list_display = ['name', 'photo_img', 'website_link']
 
-    @classmethod
     def website_link(self, obj):
         return '<a href="{0}" target="new">{0}</a>'.format(obj.website)
 
     website_link.allow_tags = True
     website_link.short_description = 'website'
 
-    @classmethod
     def photo_img(self, obj):
         return '<img width="32px" src="{}" />'.format(obj.photo)
 
